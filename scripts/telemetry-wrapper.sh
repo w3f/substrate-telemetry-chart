79f91d886fb18fcf4092057beddcb42a127686e3
Nexus2k <philipp@nexus-informatik.ch>
2021-10-26 15:36:09 +0200

    Fix telmetry-wrapper script

--- scripts/telemetry-wrapper.sh
+++ scripts/telemetry-wrapper.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Start the first process
-/usr/local/bin/telemetry_core --listen 0.0.0.0:8000 2>&1 &
+/usr/local/bin/telemetry_core 2>&1 &
 P1=$!
 
-sleep 10;
+sleep 2;
 
 # Start the second process
-/usr/local/bin/telemetry_shard --core 127.0.0.1:8000 2>&1 &
+/usr/local/bin/telemetry_shard 2>&1 &
 P2=$!
   
 # Wait for any process to exit
