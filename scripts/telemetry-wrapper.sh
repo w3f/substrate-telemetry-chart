ab5e66cdd8e4e9fe88ff902bb9da7f4df01ad2a9
Nexus2k <philipp@nexus-informatik.ch>
2021-10-26 15:06:52 +0200

    Explicitly define listen address

--- scripts/telemetry-wrapper.sh
+++ scripts/telemetry-wrapper.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 # Start the first process
-/usr/local/bin/telemetry_core 2>&1 &
-  
+/usr/local/bin/telemetry_core --listen 0.0.0.0:8000 2>&1 &
+P1=$!
+
+sleep 10;
+
 # Start the second process
-/usr/local/bin/telemetry_shard 2>&1 &
+/usr/local/bin/telemetry_shard --core 127.0.0.1:8000 2>&1 &
+P2=$!
   
 # Wait for any process to exit
-wait -n
+wait $P1 $P2
   
 # Exit with status of process that exited first
 exit $?
