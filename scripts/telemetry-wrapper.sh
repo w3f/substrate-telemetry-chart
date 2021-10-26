a4880ea29c1b8280ceb5a087b6e20d7525107aa9
Nexus2k <philipp@nexus-informatik.ch>
2021-10-26 14:40:29 +0200

    Log to stdout

--- scripts/telemetry-wrapper.sh
+++ scripts/telemetry-wrapper.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Start the first process
-/usr/local/bin/telemetry_core &
+/usr/local/bin/telemetry_core 2>&1 &
   
 # Start the second process
-/usr/local/bin/telemetry_shard &
+/usr/local/bin/telemetry_shard 2>&1 &
   
 # Wait for any process to exit
 wait -n
